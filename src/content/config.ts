d9d5d2cfe63457ef139b0b13fcd7b7ca63677782
Steele Shreve <94927484+steelesh@users.noreply.github.com>
2024-12-09 10:40:59 -0600

    refactor: remove work collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,15 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content'
 
 const postCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		draft: z.boolean().optional(),
-		title: z.string(),
-		pubDate: z.date(),
-		description: z.string(),
-	}),
-});
-
-const workCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		draft: z.boolean().optional(),
-		title: z.string(),
-		pubDate: z.date(),
-		description: z.string(),
-	}),
-});
+    type: 'content',
+    schema: z.object({
+        draft: z.boolean().optional(),
+        title: z.string(),
+        pubDate: z.date(),
+        description: z.string(),
+    }),
+})
 
 export const collections = {
-	posts: postCollection,
-	works: workCollection,
-};
+    posts: postCollection,
+}
